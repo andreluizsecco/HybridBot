e127e8ea9415d3bc1aa41035e1fc2df5e4d9a995
André Secco <andreluizsecco@yahoo.com.br>
2018-02-11 17:30:48 -0200

    Initial commit

---
+++ HybridBot/Dialogs/QnADialog.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System;
+
+namespace HybridBot.Dialogs
+{
+    [Serializable]
+    [QnAMaker("{SubscriptionKey}", "{KnowledgeBaseId}", "Não entendi qual sua dúvida, pode explicar melhor?", 0.50, 3)]
+    public class QnADialog : QnAMakerDialog { }
+}
---
+++ HybridBot/Dialogs/RootDialog.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HybridBot.Dialogs
+{
+    [Serializable]
+    [LuisModel("{ModelId}", "{SubscriptionKey}")]
+    public class RootDialog : LuisDialog<object>
+    {
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.Forward(new QnADialog(), AfterQnADialog, context.Activity, CancellationToken.None);
+        }
+
+        private async Task AfterQnADialog(IDialogContext context, IAwaitable<object> result)
+        {
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Saudacao")]
+        public async Task Saudacao(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Bem-vindo(a) ao Hotel! Sou o seu atendente virtual, o que deseja?");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Reserva")]
+        public async Task Reserva(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Ótimo! Esses são os quartos que estão disponíveis...");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("ServicoQuarto")]
+        public async Task ServicoQuarto(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Entendi, iremos enviar nosso colaborador, qual é o número do seu quarto?");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Despertador")]
+        public async Task Despertador(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Certo, pode contar comigo, não irei dormir. Que horário deseja ser acordado?");
+            context.Wait(MessageReceived);
+        }
+    }
+}
